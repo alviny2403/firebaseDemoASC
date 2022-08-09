effe8535a734f657d330730055383a71313f8301
alviny2403 <alvinyan12345@gmail.com>
2022-08-09 15:52:54 -0400

    added code

--- firebase.js
+++ firebase.js
@@ -1,15 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
 import {
-  getDatabase,
-  ref,
-  push,
-  set,
-  onValue,
+  getDatabase, // to get database from firebase
+  ref,  // give specific reference in out database
+  push, // create new location in given reference
+  set,  // adds data to our database
+  onValue,  // reads data from our database
 } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
 // Your web app's Firebase configuration
 // ENTER FIREBASE CONFIG HERE
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDV5bTieMqx_iUawGGlkGoci1odn_2d9cg",
+  authDomain: "fir-demo-28e08.firebaseapp.com",
+  projectId: "fir-demo-28e08",
+  storageBucket: "fir-demo-28e08.appspot.com",
+  messagingSenderId: "522655058063",
+  appId: "1:522655058063:web:c8b105f782d535a3ed48fa",
+  measurementId: "G-8NHY15X2RK"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 import { ref, push, set, database, onValue } from "./firebase.js";
 
 //get elements from index.html
+let userInput= document.getElementById("itemInput")
+let submitBtn= document.getElementById("submitBtn")
+let itemsContainer= document.getElementById("itemsContainer")
 
 //refrence a part of our database
+let itemsRef= ref(database, "items")
 
 //add item to database
+submitBtn.onclick= (event)=>{
+    event.preventDefault()
+    let userInputValue= userInput.value
+    userInput.value= ""
+
+    let newItemsRef= push(itemsRef)
+    set(newItemsRef, userInputValue)
+}
 
 //show items on firebase in our items container
+onValue(itemsRef, (snapshot)=>{
+    itemsContainer.innerHTML= ""
+    let data= snapshot.val()
+    console.log(data)
+    for (let key in data){
+        console.log(data[key])
+
+        let newLi= document.createElement("li")
+        newLi.innerHTML= data[key]
+
+        itemsContainer.append(newLi)
+    }
+})
